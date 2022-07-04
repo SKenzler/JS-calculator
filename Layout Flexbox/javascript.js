64646bee9c3a466523a0215418aa9874634fc966
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2022-07-04 11:41:23 +0800

    Change layout and add skill icons

---
+++ Layout Flexbox/javascript.js
@@ -0,0 +1,11 @@
+let x = "Amazing";
+console.log(x);
+
+let shoes = ['Nikes', 'Converse', 'Vans'];
+let firstShoe = shoes[0];
+
+function printShoe(shoe) {
+    document.write("Your shoes is " + shoe);
+}
+
+printShoe(firstShoe);
