dbc9145ba39f91cf4dcd7c0c1addb8b7d97ae320
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2022-10-01 15:23:47 +0800

    Add JavaScript to screen and button
    Start adding functionality to screen and buttons

--- JS-calculator/main.js
+++ JS-calculator/main.js
@@ -1,10 +1,13 @@
-// select all buttons on the calculator
+// select all buttons on the calculator and the display screen
 
-const keys = document.querySelector('.calculator-btns')
+const keys = document.querySelectorAll('.btn');
+const screen = document.querySelector('.calculator-display');
 
 // add listener 
 
-keys.addEventListener('click', event => {
+keys.forEach(function (button){
+    button.addEventListener('click', function(event){
     
     console.log('hello');
-})
+    })
+});
