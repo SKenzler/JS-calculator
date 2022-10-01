3519c73c5db26fd62ffaaff983fc722b4f94f4a7
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2022-10-01 14:48:30 +0800

    Add zero to calculator display
    Add zero to the calculator display as default display

--- JS-calculator/main.js
+++ JS-calculator/main.js
@@ -1,5 +1,10 @@
-const keys = document.querySelector('calculator-btns')
+// select all buttons on the calculator
+
+const keys = document.querySelector('.calculator-btns')
+
+// add listener 
 
 keys.addEventListener('click', event => {
-    console.log("Hello");
+    
+    console.log('hello');
 })
