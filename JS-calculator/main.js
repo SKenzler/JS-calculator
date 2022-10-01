a1d257e9e2233f04de3901f501c44f4f4e2c0ade
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2022-10-01 19:24:04 +0800

    Add equal & clear functionality
    Add functionality to the equal and clear button

--- JS-calculator/main.js
+++ JS-calculator/main.js
@@ -1,13 +1,39 @@
-// select all buttons on the calculator and the display screen
 
-const keys = document.querySelectorAll('.btn');
-const screen = document.querySelector('.calculator-display');
+    // select all buttons on the calculator and the display screen
 
-// add listener 
+    const keys = document.querySelectorAll('.btn');
+    const equalKey = document.querySelector('.btn-equals');
+    const clearKey = document.querySelector('.btn-clear');
+    const screen = document.querySelector('.calculator-display');
 
-keys.forEach(function (button){
-    button.addEventListener('click', function(event){
-    
-    console.log('hello');
-    })
-});
+    // add listener to the operations and number button and display value on the calculator display
+
+    keys.forEach(function (button){
+        button.addEventListener('click', function(event){
+
+            let value = event.target.dataset.number;
+            screen.innerHTML += value;
+        })
+    });
+
+    // add listener to the equal button and display value on the calculator display
+
+    equalKey.addEventListener('click', function(event){
+        
+        let value = screen.innerHTML;
+
+        if(value === ""){
+            screen.innerHTML = "";
+        }
+
+        else{
+            let result = eval(value);
+            screen.innerHTML = result;
+        }
+    });
+
+     // add listener to the clear button and reset calculator display
+
+    clearKey.addEventListener('click', function(event){
+        screen.innerHTML = "";
+    });
